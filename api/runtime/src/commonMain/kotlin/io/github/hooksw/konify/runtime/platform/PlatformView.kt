907f3534473f1f30fecddd3c9fba8211c69c19cd
Peanuuutz <1415605913@qq.com>
2023-08-24 22:44:26 +0800

    Migrate project ID.

--- api/runtime/src/commonMain/kotlin/io/github/hooksw/konify/runtime/platform/PlatformView.kt
+++
@@ -1,7 +0,0 @@
-package io.github.hooksw.konify.runtime.platform
-
-expect class PlatformView {
-    fun addChild(platformView: PlatformView)
-
-    fun removeChild(platformView: PlatformView)
-}
--- api/runtime/src/commonMain/kotlin/io/github/hooksw/konify/runtime/state/Observer.kt
+++
@@ -1,5 +0,0 @@
-package io.github.hooksw.konify.runtime.state
-
-fun interface Observer<in T> {
-    fun accept(value: T)
-}
